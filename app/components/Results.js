980914c5092551b7851661053a7abd85cf6e7f8d
HenryGBC <henry.bravo89@gmail.com>
2017-01-17 21:17:13 -0430

    77% complete

---
+++ app/components/Results.js
@@ -0,0 +1,22 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+var styles= require('../styles');
+
+
+function puke(obj){
+	return <pre>{JSON.stringify(obj, null, ' ')} </pre>
+}
+
+
+function Results(props){
+	return (
+		<div>Results {puke(props)}</div>
+	)
+}
+
+Results.proptypes = {
+	isLoading: PropTypes.bool.isRequired,
+	playersInfo: PropTypes.array.isRequired,
+	scores: PropTypes.array.isRequired
+}
+module.exports = Results;
--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -8,6 +8,40 @@ function getUserInfo(username){
 	return axios.get('https://api.github.com/users/' + username);
 }
 
+function getRepos(username){
+	//fetch username repos
+  return axios.get('https://api.github.com/users/' + username + '/repos?per_page=100');
+}
+function getTotalstars (repos){
+  //calvulate all the stars that user has
+  return repos.data.reduce(function(prev, current){
+    return prev + current.stargazers_count 
+  }, 0)
+}
+
+function getPlayersData (player){
+  // get repos
+  // get total stars
+  // return ibject wih that data
+
+  return getRepos(player.login)
+    .then(getTotalstars)
+    .then(function(totalStars){
+      console.log(totalStars);
+      return{
+        followers: player.followers,
+        totalStars: totalStars
+      }
+    })
+}
+
+function calculateScores(players){
+  //Return an array after doing some fancy algotihms to determinate a winner
+  return [
+    players[0].followers * 3 * players[0].totalStars,
+    players[1].followers * 3 * players[1].totalStars
+  ]
+}
 var helpers = {
 	getPlayersInfo: function(players){
 		return axios.all(players.map(function(username){
@@ -17,8 +51,15 @@ var helpers = {
 				return user.data;
 			})
 		}).catch(function(err){
-      console.log('Error in getPlayersInfo', err)
-    });
+	      console.log('Error in getPlayersInfo', err)
+	    });
+	},
+	battle: function (players){
+    var playerOneData = getPlayersData(players[0]);
+    var playerTwoData = getPlayersData(players[1]);
+    return axios.all([playerOneData, playerTwoData])
+      .then(calculateScores)
+      .catch(function (err){console.log('Err: ', err)});
 	}
 };
 
